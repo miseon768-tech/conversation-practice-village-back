53723d5eec88bebede7bce511aa52fa9e0d64c38
miseon768-tech <miseon768@gmail.com>
2026-02-27 18:46:42 +0900

    Initial commit: Spring Boot Backend

---
+++ src/main/java/com/example/conversationpracticevillageback/ConversationPracticeVillageBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.conversationpracticevillageback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversationPracticeVillageBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversationPracticeVillageBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/conversationpracticevillageback/ConversationPracticeVillageBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conversationpracticevillageback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversationPracticeVillageBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
